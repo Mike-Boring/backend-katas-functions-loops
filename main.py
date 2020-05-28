fa9c9bfaff2ba9b59a72f9456c380717e33a3f63
Mike Boring <shekinah999@msn.com>
2020-05-28 14:30:14 -0600

    Had to refactor the multiply function and power function

--- main.py
+++ main.py
@@ -4,59 +4,31 @@ except for '+' and '-'.
 """
 
 __author__ = "Mike Boring"
-# See notes below power function.
+# Had a little help from Daniel Lomelino with my functions affecting the tests on the
+# power function comparison with x ** y
 
 
 def add(x, y):
     """Add two integers."""
-    result = x + y
-    return result
+    return x + y
 
 
 def multiply(x, y):
     """Multiply x with y using the add() function above."""
     result = 0
-    if x == 0 or y == 0:
-        return 0
     for _ in range(abs(y)):
-        result = add(result, abs(x))
-    if x < 0 and y < 0:
-        return result
-    if x > 0 and y > 0:
-        return result
-    if x < 0 or y < 0:
+        result = add(result, x)
+    if y < 0:
         return -result
+    return result
 
 
 def power(x, n):
     """Raise x to power n, where n >= 0, using the functions above."""
-    print(x, n)
-    result = x
-    if n == 1:
-        return x
-    if x == 0 and n > 0:
-        return 0
-    if x < 0 and n == 0:
-        return -1
-    if x == 0 and n == 0:
-        return 1
-    if x > 0 and n == 0:
-        return 1
-    if x == 1 and n > 0:
-        return 1
-    for _ in range(1, n):
-        result = multiply(abs(result), abs(x))
-    if x < 0:
-        return -result
-    if x > 0:
-        return result
-
-
-print(power(-2, 2))  # Not sure why my unit test is failing for power
-print(-2 ** 2)  # when this print to terminal proves the proper value is
-# is being generated as they match. I am printing
-#  out the paramaters in the test output so they can be
-# tested against these print functions.
+    result = 1
+    for _ in range(n):
+        result = multiply(result, x)
+    return result
 
 
 def factorial(x):
